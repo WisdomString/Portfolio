0b9fc9f97fb03e1cd2554fed7dce1036fcbfb81a
wisdom cube <remyodhner14432@gmail.com>
2021-08-28 15:17:19 +0800

    Minor preparations
    react palang nabutang ko
    video tutorial:
    https://www.youtube.com/watch?v=hQAHSlTtcmY
    
    ako pang gina limpyo ang repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import One from './One';
+import Two from './Two';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         hex
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <div><One></One></div> 
+    <div><Two></Two></div>
+  </>
+    )
 }
 
 export default App;
+
+//ang "<One>" na gitago nako sa div kay mao na ang One.js
+//ang "<Two>" mao ni ang "2nd test" 
+//kung i edit ninyo ang css sa "<div>" tags, pwede ra ninyo buhaton sa App.css pero paki butang lang ug classname 
+// example:     <div className="tucal"> <One></One> </div>  
+// tapos i call ra ninyo ang className="Tucal" sa App.css
+//pwede man pud mo mag-dungag/import ug ubang CSS maliban sa App.css , depende sa inyo ..
