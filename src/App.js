951643a90f58b788d1f9114fb72b6655d93acce6
wisdom cube <remyodhner14432@gmail.com>
2021-08-28 13:19:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>testing text</code>vince
+         hex
         </p>
         <a
           className="App-link"
