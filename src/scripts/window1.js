75735cc8012a1ac78716645637280fdfc574f437
wisdom cube <remyodhner14432@gmail.com>
2021-09-30 00:41:45 +0800

    added some neons

--- src/scripts/window1.js
+++ src/scripts/window1.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Swiping_pic from'./swiping_pic';
+import hanielpf from '../components/haneil_pf_400x400.jpg'
 
 function window1() {
     return (
@@ -17,11 +18,17 @@ function window1() {
 <div className="Haniel_space">
     <Container className="hi">
   <Row>
-    <Col sm={4} className="Side_bar_container">
-    <h1 className="Side_bar_column"> Insert Haniel's face here with roundcircle borders  </h1>
+    <Col  className="Side_bar_container">
+    <div className="Side_bar_column_Haniel"> 
+    <div className="Haniel_pf_container">
+    <div className="Outline_Hover">
+    <img src={hanielpf} width="350" height="350" className="Haniel_pf_img"></img>
+    </div>
+    </div>
+     </div>
     </Col>
     
-    <Col sm={8}>
+    <Col xs={6} fluid>
         <p>2 of 3 (wider also the main content)
         Cillum Lorem dolor Lorem in enim duis qui nostrud in anim nulla. Ipsum est mollit ullamco culpa nisi i
         n commodo sunt consectetur elit voluptate est labore. Magna laborum ad consequat officia sint quis con
@@ -73,12 +80,18 @@ function window1() {
 
 <div className="Nephilim_space">
 <Container className="hi">
-  <Row>
-    <Col sm={4} className="Side_bar_container">
-    <h1 className="Side_bar_column"> Insert Nephilim's face here with roundcircle borders</h1>
+<Row>
+    <Col  className="Side_bar_container">
+    <div className="Side_bar_column_Haniel"> 
+    <div className="Haniel_pf_container">
+    <div className="Outline_Hover">
+    <img src={hanielpf} width="350" height="350" className="Haniel_pf_img"></img>
+    </div>
+    </div>
+     </div>
     </Col>
     
-    <Col sm={8}>
+    <Col xs={6} fluid>
         <p>2 of 3 (wider also the main content)
         Cillum Lorem dolor Lorem in enim duis qui nostrud in anim nulla. Ipsum est mollit ullamco culpa nisi i
         n commodo sunt consectetur elit voluptate est labore. Magna laborum ad consequat officia sint quis con
@@ -130,12 +143,18 @@ function window1() {
 
 <div className="Dullah_space">
 <Container className="hi">
-  <Row>
-    <Col sm={4} className="Side_bar_container">
-    <h1 className="Side_bar_column">Insert Dullah's face here with roundcircle borders</h1>
+<Row>
+    <Col  className="Side_bar_container">
+    <div className="Side_bar_column_Haniel"> 
+    <div className="Haniel_pf_container">
+    <div className="Outline_Hover">
+    <img src={hanielpf} width="350" height="350" className="Haniel_pf_img"></img>
+    </div>
+    </div>
+     </div>
     </Col>
     
-    <Col sm={8}>
+    <Col xs={6} fluid>
         <p>2 of 3 (wider also the main content)
         Cillum Lorem dolor Lorem in enim duis qui nostrud in anim nulla. Ipsum est mollit ullamco culpa nisi i
         n commodo sunt consectetur elit voluptate est labore. Magna laborum ad consequat officia sint quis con
@@ -186,12 +205,18 @@ function window1() {
 
 <div className="Dianna_space">
 <Container className="hi">
-  <Row>
-    <Col sm={4} className="Side_bar_container">
-    <h1 className="Side_bar_column">Insert Dianna's face here with roundcircle borders</h1>
+<Row>
+    <Col  className="Side_bar_container">
+    <div className="Side_bar_column_Haniel"> 
+    <div className="Haniel_pf_container">
+    <div className="Outline_Hover">
+    <img src={hanielpf} width="350" height="350" className="Haniel_pf_img"></img>
+    </div>
+    </div>
+     </div>
     </Col>
     
-    <Col sm={8}>
+    <Col xs={6} fluid>
         <p>2 of 3 (wider also the main content)
         Cillum Lorem dolor Lorem in enim duis qui nostrud in anim nulla. Ipsum est mollit ullamco culpa nisi i
         n commodo sunt consectetur elit voluptate est labore. Magna laborum ad consequat officia sint quis con
